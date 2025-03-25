497b6508fb4bfe4ae8657e4c555a929a5e4c729f
lavinialei2 <lavinialei@g.ucla.edu>
2025-03-24 23:35:00 -0700

    packaged app

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const createWindow = () => {
     frame: false,
     resizable: false,
     transparent: true,
-    backgroundColor: '#00FFFFFF',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
